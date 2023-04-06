31aad67b5bca784f6e3f2a7279c2086375c22ff0
gunnar-miklis <gunnar.miklis@gmail.com>
2023-04-06 20:27:29 +0200

    Introduce: hasKey conditions | Resolve: some character reset issues | Add: transition screen before next level

--- scr/variables.js
+++ scr/variables.js
@@ -3,6 +3,11 @@ const WIDTH = 800;
 const HEIGHT = 500;
 let fontRegular, fontMedium, fontBold, fontBlack;
 
+// NOTE: Character related 
+let doCharacterReset = false;
+let characterHasKey = false;
+let counter = 0;
+
 // NOTE: Game start related 
 let buttonStartGame;
 let gameIsStarted = false;
@@ -18,9 +23,13 @@ function startLevel() {
         activeLevel = level.construct;
         // also set level id
         activeLevelId = level.id;
+
+        if ( level.hasKey ) { characterHasKey = level.hasKey }
         };
         // reset characters position
         game.character.resetCharacter();
+        characterHasKey = false;
+        doCharacterReset = true;
     })
 }
 
@@ -31,15 +40,22 @@ let selectedLevel; // dropdown menu
 // add levels to the dropdown menu, but ONLY, when they have been started already.
 function addFinishedLevelToDropdown() {
     // create <option> tags
-    levelList.forEach( (level) => { if ( level.isStarted ) selectedLevel.option( level.name, level.name ) });
+    // levelList.forEach( (level) => { if ( level.isStarted ) selectedLevel.option( level.name, level.name ) });
+    levelList.forEach( (level) => { selectedLevel.option( level.description, level.name ) }); // TESTING 
 }
 function nextLevel() {
+    // transition screen
+    game.printTextCentered( 'Success!',1000 );
+
     // increment level number
     activeLevelId++;
+
     // set level isStarted to: true
     if( activeLevelId < levelList.length ) levelList[activeLevelId].isStarted = true;
+
     // refresh dropwdown menu
     addFinishedLevelToDropdown();
+
     // loop leve list again
     levelList.forEach( (level) => {
         // pick next level
@@ -49,11 +65,11 @@ function nextLevel() {
             // set dropdown selection to current game
             document.querySelector('#menu select').selectedIndex = activeLevelId;
         }
+        
+        if ( level.hasKey ) { characterHasKey = level.hasKey }
         // reset characters position
         // needs to be called as most-last, AFTER events(). using condition in draw()
         doCharacterReset = true;
     })
-}
 
-// NOTE: Character related 
-let doCharacterReset = false;
+}
