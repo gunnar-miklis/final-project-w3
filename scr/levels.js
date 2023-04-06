f6887e27cec9676cee780070b53da590e802bc1e
gunnar-miklis <gunnar.miklis@gmail.com>
2023-04-06 20:25:40 +0200

    Completely revised level logic | Implement: migrated platform interactions | Changes to hasKey=true levels

--- scr/levels.js
+++ scr/levels.js
@@ -9,7 +9,6 @@ class Levels { // DONE
         this.platform1 = new Platform( 200, 400, 'black' );
         this.platform2 = new Platform( 250, 200, 'black' );
         this.platform3 = new Platform( 450, 200, 'black' );
-        this.platform4;
     }
 
     printTextLevelTitle(insertTitle) {
@@ -31,37 +30,24 @@ class Levels { // DONE
         this.platform1.update();
         this.platform2.update();
         this.platform3.update();
-        if ( this.platform4) this.platform4.update();
     }
 
-    events() {
-        this.key.onCollision(game.character);
-        this.exit.onCollision(game.character);
-        this.trap1.onCollision(game.character);
-        this.trap2.onCollision(game.character);
-        this.platform1.onCollision(game.character);
-        this.platform2.onCollision(game.character);
-        this.platform3.onCollision(game.character);
-        if ( this.platform4) this.platform4.onCollision(game.character);
+    events(character) {
+        this.key.stayOnCollision(character);
+        this.exit.stayOnCollision(character);
+        this.trap1.stayOnCollision(character);
+        this.trap2.stayOnCollision(character);
+        this.platform1.stayOnCollision(character);
+        this.platform2.stayOnCollision(character);
+        this.platform3.stayOnCollision(character);
     }
 }
 // NOTE: L 0 
 class Level0 extends Levels {
+
     constructor() {
         super();
-        this.title = "dancin' potato\nohhh yeahhh !!!\n\n\ntiny\npotato\nwohohhh!";
-        this.key = new Platform( 50, 450, 'black' );
-        this.exit = new Platform( 150, 400, 'black' );
-        this.trap1 = new Platform( 250, 350, 'black' );
-        this.trap2 = new Platform( 350, 300, 'black' );
-        this.platform1 = new Platform( 450, 350, 'black' );
-        this.platform2 = new Platform( 550, 400, 'black' );
-        this.platform3 = new Platform( 650, 450, 'black' );
-    }
-
-    events() {
-        super.events();
-        game.character.hasKey = true;
+        this.title = "dancin' potato ohhh yeahhh !!!\ntiny potato wohohhh!";
     }
 }
 // NOTE: L1, find the exit 
@@ -70,14 +56,13 @@ class Level1 extends Levels { // DONE
     constructor() {
         super();
         this.title = 'find the exit';
-        this.key = new Platform( 650, 300, 'black' );
         this.trap1 = new Platform( 500, 491, 'black' );
         this.trap2 = new Platform( 350, 200, 'black' );
     }
 
-    events() {
-        super.events();
-        game.character.hasKey = true;
+    events(character) {
+        super.events(character);
+        this.exit.winOnCollision(character);
     }
 }
 // NOTE: L2, find a way to access the exit 
@@ -89,33 +74,51 @@ class Level2 extends Levels { // DONE
         this.trap1 = new Platform( 500, 491, 'black' );
         this.trap2 = new Platform( 350, 200, 'black' );
     }
+
+    events(character) {
+        super.events(character);
+        this.exit.winOnCollision(character);
+        this.key.giveKeyOnCollision(character);
+    }
 }
-// NOTE: L3, wait for the it 
+// NOTE: L3, the floor is lava 
 class Level3 extends Levels { // DONE 
-
-    constructor() {
-        super();
-        this.title = "wait for the it";
-    }
-
-    events() {
-        super.events();
-        if ( counter <= 5 ) {
-            game.character.hasKey = false
-        } else {
-            game.character.hasKey = true;
-        }
-    }
-}
-// NOTE: L4, the floor is lava 
-class Level4 extends Levels { // DONE 
 
     constructor() {
         super();
         this.title = 'the floor is lava';
         this.trap2 = new Platform( 350, 200, 'black' );
     }
+
+    events(character) {
+        super.events(character);
+        this.exit.winOnCollision(character);
+        this.key.giveKeyOnCollision(character);
+        this.trap1.deathOnCollision(character);
+    }
 }
+// NOTE: L4, wait for the it 
+class Level4 extends Levels { // DONE 
+
+    constructor() {
+        super();
+        this.title = "wait for the it";
+        this.trap1 = new Platform( 500, 491, 'black' );
+        this.trap2 = new Platform( 350, 200, 'black' );
+    }
+
+    events(character) {
+        super.events(character);
+        this.exit.winOnCollision(character);
+        this.key.giveKeyOnCollision(character);
+        if ( counter <= 5 ) {
+            characterHasKey = false
+        } else {
+            characterHasKey = true;
+        }
+    }
+}
+
 // NOTE: L5, more difficult 
 class Level5 extends Levels { // DONE 
 
@@ -123,9 +126,42 @@ class Level5 extends Levels { // DONE
         super();
         this.title = "more difficult";
     }
+
+    events(character) {
+        super.events(character);
+        this.exit.winOnCollision(character);
+        this.key.giveKeyOnCollision(character);
+        
+        this.trap1.deathOnCollision(character);
+        this.trap2.deathOnCollision(character);
+    }
 }
-// NOTE: L6, don't trust your eyes
+// NOTE: L6, start is exit 
 class Level6 extends Levels { // DONE 
+
+    constructor() {
+        super();
+        this.title = "Start is exit";
+        this.exit = new Exit( -70, 491, 'white' );
+        this.platform4 = new Platform( 50, 300, 'black' );
+    }
+
+    update() {
+        super.update();
+        this.platform4.update();
+    }
+
+    events(character) {
+        super.events(character);
+        this.exit.winOnCollision(character);
+        this.key.giveKeyOnCollision(character);
+        this.trap1.deathOnCollision(character);
+        this.trap2.deathOnCollision(character);
+        this.platform4.stayOnCollision(character);
+    }
+}
+// NOTE: L7, don't trust your eyes
+class Level7 extends Levels { // DONE 
 
     constructor() {
         super();
@@ -133,15 +169,29 @@ class Level6 extends Levels { // DONE
         this.trap1 = new Trap( 500, 491, 'black' );
         this.trap2 = new Trap( 350, 200, 'black' );
     }
+
+    events(character) {
+        super.events(character);
+        this.exit.winOnCollision(character);
+        this.key.giveKeyOnCollision(character);
+        this.trap1.deathOnCollision(character);
+        this.trap2.deathOnCollision(character);
+    }
 }
-// NOTE: L7, start is exit 
-class Level7 extends Levels { // DONE 
+// NOTE: L8, click on That level 
+class Level8 extends Levels { // DONE 
 
     constructor() {
         super();
-        this.title = "start is exit";
-        this.exit = new Exit( -89, 491, 'white' );
-        this.platform4 = new Platform( 50, 300, 'black' );
+        this.title = "click on That level";
+    }
+
+    events(character) {
+        super.events(character);
+        this.exit.winOnCollision(character);
+        this.trap1.deathOnCollision(character);
+        this.trap2.deathOnCollision(character);
+        document.querySelector('h2').addEventListener('click',()=>{ characterHasKey = true })
     }
 }
 
