85b7b5c6e378d4b2d57f2e4d8e861c47363b637b
gunnar-miklis <gunnar.miklis@gmail.com>
2023-04-07 22:29:34 +0200

    Add: comments | Re-structure: code to make it more readable (left side code, right side comments)

--- sketch.js
+++ sketch.js
@@ -1,49 +1,51 @@
 // NOTE: create game and load assets 
-const game = new Game();
+const game = new Game();                                                // construct game
 
 function preload() { // DONE 
-  game.preloadImages();
-  game.preloadFonts();
+    game.preloadImages();
+    game.preloadFonts();
 }
 
 // NOTE: setup items 
 function setup() { // DONE 
-  // create canvas
-  createCanvas( WIDTH, HEIGHT ).parent('#p5-div');
-  
-  // create dropdown menu
-  selectedLevel = createSelect().parent('#menu');
-  selectedLevel.class('dropdown'); // assign class
-  addFinishedLevelToDropdown();
+    createCanvas( WIDTH, HEIGHT ).parent('#p5-div');                    // create canvas
 
-  // create start button
-  buttonStartGame = createButton('Start').parent('#menu').mousePressed( startLevel );
-  buttonStartGame.class('start-btn'); // assign class
+    selectedLevel = createSelect()                                      // create dropdown menu
+                        .parent('#menu')                                // anchor into menu
+                        .class('dropdown');                             // assign class  for styling
+    addFinishedLevelToDropdown();                                       // refresh/reload dropdown menu
+
+    buttonStartGame = createButton('Start')                             // create start button
+                        .parent('#menu')                                // anchor into menu
+                        .class('start-btn')                             // assign class for styling
+                        .mousePressed( startLevel );                    // enable mouse click functionality
 }
 
 // NOTE: draw items 
 function draw() { // DONE 
-  if ( gameIsStarted ) {
-    clear();
-    game.placeCharacter(); // COMMENT: maybe draw the character inside the level() (at each level re-start), instead of global draw 
-    game.createLevel(activeLevel); // based on loaded level (from levelList), draw level
-    game.eventListener(activeLevel);
-    if ( doCharacterReset ) {
-      game.character.resetCharacter();
-      characterHasKey = levelList[activeLevelId].hasKey
-      doCharacterReset = false;
+    if ( gameIsStarted ) {
+        clear();                                                        // remove past frames
+
+        game.placeCharacter();                                          // draw character // COMMENT: it might be better to call the character with each level in the Levels() class. 
+        game.createLevel(activeLevel);                                  // draw level based on loaded active-level
+        game.eventListener(activeLevel);                                // enable functionality and interactions inside levels
+
+        if ( doCharacterReset ) {                                       // has to stay at the very last position of draw in order to do the reset AFTER all other events, can be triggered individually
+            game.character.resetCharacter();                            // call character reset function to reset: position, velocity, allow jumping, disable isAllowedToPass
+            characterHasKey = levelCollection[activeLevelid].hasKey;    // set intial value for hasKey (value comes from levelCollection{} according to active-level-id )
+            doCharacterReset = false;                                   // disable condition
+        }
     }
-  }
 }
 
 // NOTE: extra functionality 
 function keyPressed() { // DONE 
-  if ( keyCode === 39 ) game.character.moveRight();
-  if ( keyCode === 37 ) game.character.moveLeft();
-  if ( keyCode === 38 ) game.character.jump();
-  if ( keyCode === 40 ) game.character.moveDown();
+    if ( keyCode === 39 ) game.character.moveRight();
+    if ( keyCode === 37 ) game.character.moveLeft();
+    if ( keyCode === 38 ) game.character.jump();
+    if ( keyCode === 40 ) game.character.moveDown();
 }
 
 function sleep(duration) { // DONE 
-  return new Promise( resolve => setTimeout(resolve, duration) );
+    return new Promise( resolve => setTimeout(resolve, duration) );     // allows to wait for a certain amount of time, before continue with code flow
 }
