99688fe9014dfa8567d9a701a3e535306074dc28
gunnar-miklis <gunnar.miklis@gmail.com>
2023-04-07 22:38:47 +0200

    Add: comments | Re-structure: code to make it more readable (left side code, right side comments) | Add: ability to load the instructions from levelCollection{} instead of manual input | Remove: redundant code | Rename: some variables to make their function easier to understand | Simplify: code for level-4 win-condition

--- scr/levels.js
+++ scr/levels.js
@@ -1,28 +1,27 @@
 class Levels { // DONE 
 
-    constructor() {
-        this.title;
-        this.key = new Key( 650, 300, 'blue' );
-        this.exit = new Exit( 50, 300, 'green' );
+    constructor( instructions ) {
+        this.instructions = instructions;                                   // load level instructions from levelCollection{}, to automatically display them for their corresponding level
+        this.key = new Key( 650, 300, 'blue' );                             // "default" platform structure, will basically stay the same all the time.
+        this.exit = new Exit( 50, 300, 'green' );                           // containing: exit, key, 2x trap, 3x platform
         this.trap1 = new Trap( 500, 491, 'red' );
         this.trap2 = new Trap( 350, 200, 'red' );
         this.platform1 = new Platform( 200, 400, 'black' );
         this.platform2 = new Platform( 250, 200, 'black' );
-        this.platform3 = new Platform( 450, 200, 'black' );
+        this.platform3 = new Platform( 450, 200, 'black' );                 
+        //this.character = new Character();                                 // COMMENT: it might be better to call the character here, instead of in Game() class. with each game construction. not sure though. 
     }
 
-    printTextLevelTitle(insertTitle) {
-        if ( gameIsStarted ) {
-            fill('black');
-            textFont(fontRegular);
-            textSize(25);
-            textAlign(CENTER);
-            text(insertTitle,WIDTH/2,200+40);
-        }
+    printLevelInstructions( insertTitle ) {                                 // function to print level instructions (hints) in the center
+            fill( 'black' );
+            textFont( fontRegular );
+            textSize( 25 );
+            textAlign( CENTER );
+            text( insertTitle, WIDTH/2, 200 + 40 );
     }
 
-    update() {
-        this.printTextLevelTitle(this.title);
+    update() {                                                              // functions to draw platforms on canvas
+        this.printLevelInstructions( this.instructions );
         this.key.update();
         this.exit.update();
         this.trap1.update();
@@ -30,91 +29,82 @@ class Levels { // DONE
         this.platform1.update();
         this.platform2.update();
         this.platform3.update();
+        // this.character.update();                                         // COMMENT: character draw() 
     }
 
-    events(character) {
-        this.key.stayOnCollision(character);
-        this.exit.stayOnCollision(character);
-        this.trap1.stayOnCollision(character);
-        this.trap2.stayOnCollision(character);
-        this.platform1.stayOnCollision(character);
-        this.platform2.stayOnCollision(character);
-        this.platform3.stayOnCollision(character);
+    events( character ) {                                                   // stayOnCollision() functions needs to be available anytime, add them to draw()
+        this.key.stayOnCollision( character );
+        this.exit.stayOnCollision( character );
+        this.trap1.stayOnCollision( character );
+        this.trap2.stayOnCollision( character );
+        this.platform1.stayOnCollision( character );
+        this.platform2.stayOnCollision( character );
+        this.platform3.stayOnCollision( character );
     }
 }
 // NOTE: L 0 
-class Level0 extends Levels {
-
-    constructor() {
-        super();
-        this.title = "dancin' potato ohhh yeahhh !!!\ntiny potato wohohhh!";
-    }
-}
+class Level0 extends Levels {}
 // NOTE: L1, find the exit 
 class Level1 extends Levels { // DONE 
 
-    constructor() {
-        super();
-        this.title = 'find the exit';
-        this.trap1 = new Platform( 500, 491, 'black' );
-        this.trap2 = new Platform( 350, 200, 'black' );
+    constructor( instructions ) {
+        super( instructions );
+        this.trap1 = new Platform( 500, 491, 'black' );                     // trap1 will be replaced by just a regular platform
+        this.trap2 = new Platform( 350, 200, 'black' );                     // trap2 will be replaced by just a regular platform as well
     }
 
-    events(character) {
-        super.events(character);
-        this.exit.winOnCollision(character);
+    events( character ) {
+        super.events( character );
+        this.exit.winOnCollision( character );                              // add winOnCollision() event to draw()
     }
 }
 // NOTE: L2, find a way to access the exit 
 class Level2 extends Levels { // DONE 
 
-    constructor() {
-        super();
-        this.title = 'find a way to access the exit';
+    constructor( instructions ) {
+        super( instructions );
         this.trap1 = new Platform( 500, 491, 'black' );
         this.trap2 = new Platform( 350, 200, 'black' );
     }
 
-    events(character) {
-        super.events(character);
-        this.exit.winOnCollision(character);
-        this.key.giveKeyOnCollision(character);
+    events( character ) {
+        super.events( character );
+        this.exit.winOnCollision( character );
+        this.key.giveKeyOnCollision( character );                           // add giveKeyOnCollision() event to draw(), so character can collect key
     }
 }
 // NOTE: L3, the floor is lava 
 class Level3 extends Levels { // DONE 
 
-    constructor() {
-        super();
-        this.title = 'the floor is lava';
-        this.trap2 = new Platform( 350, 200, 'black' );
+    constructor( instructions ) {
+        super( instructions );
+        this.trap2 = new Platform( 350, 200, 'black' );                     // replace only 1 trap this time. the other one will be drawed
     }
 
-    events(character) {
-        super.events(character);
-        this.exit.winOnCollision(character);
-        this.key.giveKeyOnCollision(character);
-        this.trap1.deathOnCollision(character);
+    events( character ) {
+        super.events( character );
+        this.exit.winOnCollision( character );
+        this.key.giveKeyOnCollision( character );
+        this.trap1.deathOnCollision( character );                           // add deathOnCollision() event to draw(), so character may die on trap
     }
 }
 // NOTE: L4, wait for the it 
 class Level4 extends Levels { // DONE 
 
-    constructor() {
-        super();
-        this.title = "wait for the it";
+    constructor( instructions ) {
+        super( instructions );
         this.trap1 = new Platform( 500, 491, 'black' );
         this.trap2 = new Platform( 350, 200, 'black' );
     }
 
-    events(character) {
-        super.events(character);
-        this.exit.winOnCollision(character);
-        this.key.giveKeyOnCollision(character);
-        if ( counter <= 5 ) {
-            characterHasKey = false
-        } else {
-            characterHasKey = true;
+    events( character ) {
+        super.events( character );
+        this.exit.winOnCollision( character );
+        this.key.giveKeyOnCollision( character );                           // add giveKeyOnCollision() event to draw(), so character can collect key, BUT...
+        if ( isAllowedToPass ) {                                            // ...this time, the character will only collect a key when they're "allowed to pass".
+            characterHasKey = true;                                         // isAllowedToPass, will be triggered by a frameCount of ~5 seconds.
+        } else {                                                            // so, the character has to stay for ~5s on the platform. after it'll be "allowed to pass" = collect the key.
+            characterHasKey = false;                                        // COMMENT: 'else' here, is probably not necessary but makes the code easier to understand 
         }
     }
 }
@@ -122,78 +112,67 @@ class Level4 extends Levels { // DONE
 // NOTE: L5, more difficult 
 class Level5 extends Levels { // DONE 
 
-    constructor() {
-        super();
-        this.title = "more difficult";
-    }
-
-    events(character) {
-        super.events(character);
-        this.exit.winOnCollision(character);
-        this.key.giveKeyOnCollision(character);
+    events( character ) {
+        super.events( character );                                          // all "special" platforms are active here
+        this.exit.winOnCollision( character );                              // win
+        this.key.giveKeyOnCollision( character );                           // key
         
-        this.trap1.deathOnCollision(character);
-        this.trap2.deathOnCollision(character);
+        this.trap1.deathOnCollision( character );                           // death/reset
+        this.trap2.deathOnCollision( character );
     }
 }
 // NOTE: L6, start is exit 
 class Level6 extends Levels { // DONE 
 
-    constructor() {
-        super();
-        this.title = "Start is exit";
-        this.exit = new Exit( -70, 491, 'white' );
-        this.platform4 = new Platform( 50, 300, 'black' );
+    constructor( instructions ) {
+        super( instructions );
+        this.exit = new Exit( 0, 491, 'white' );                            // exit platform will be moved to a different position and becomes invisible 'white' (on white canvas)
+        this.platform4 = new Platform( 50, 300, 'black' );                  // initial exit platform position will be replaced by just a regular platform-4
     }
 
     update() {
         super.update();
-        this.platform4.update();
+        this.platform4.update();                                            // need to draw() new platform-4 (since it's not in the default level setup)
     }
 
-    events(character) {
-        super.events(character);
-        this.exit.winOnCollision(character);
-        this.key.giveKeyOnCollision(character);
-        this.trap1.deathOnCollision(character);
-        this.trap2.deathOnCollision(character);
-        this.platform4.stayOnCollision(character);
+    events( character ) {
+        super.events( character );
+        this.exit.winOnCollision( character );
+        this.key.giveKeyOnCollision( character );
+        this.trap1.deathOnCollision( character );
+        this.trap2.deathOnCollision( character );
+        this.platform4.stayOnCollision(character);                          // stayOnColliision() needs to be added for platform-4 as well
     }
 }
 // NOTE: L7, don't trust your eyes
 class Level7 extends Levels { // DONE 
 
-    constructor() {
-        super();
-        this.title = "don't trust your eyes";
-        this.trap1 = new Trap( 500, 491, 'black' );
+    constructor( instructions ) {
+        super( instructions );
+        this.trap1 = new Trap( 500, 491, 'black' );                         // trap platforms become color 'black' instead default 'red'...
         this.trap2 = new Trap( 350, 200, 'black' );
     }
 
-    events(character) {
-        super.events(character);
-        this.exit.winOnCollision(character);
-        this.key.giveKeyOnCollision(character);
-        this.trap1.deathOnCollision(character);
-        this.trap2.deathOnCollision(character);
+    events( character ) {
+        super.events( character );
+        this.exit.winOnCollision( character );
+        this.key.giveKeyOnCollision( character );
+        this.trap1.deathOnCollision( character );                           // ...to make them "appear" disabled but in fact, deathOnCollision() will still be triggerd.
+        this.trap2.deathOnCollision( character );
     }
 }
 // NOTE: L8, click on That level 
 class Level8 extends Levels { // DONE 
 
-    constructor() {
-        super();
-        this.title = "press on That level";
-    }
-
-    events(character) {
-        super.events(character);
-        this.exit.winOnCollision(character);
-        this.trap1.deathOnCollision(character);
-        this.trap2.deathOnCollision(character);
-        document.querySelector('.title h2').addEventListener('click',()=>{ characterHasKey = true })
+    events( character ) {
+        super.events( character );                                          // giveKeyOnCollision() is disabled. so, there's no way to collect a key on any platform...
+        this.exit.winOnCollision( character );
+        this.trap1.deathOnCollision( character );
+        this.trap2.deathOnCollision( character );
+        document.querySelector( '.title h2' )                               // ...but clicking on h2 "That level, again?"...
+                    .addEventListener( 'click', ()=>{
+                        characterHasKey = true;                             // ...will give the character the key.
+                    });
     }
 }
-
-
 // TODO: Add more levels 
